115711669f82fcdbfa0099a78ba1b432c2c7e677
BacancyAnkita <ankita.rathod@bacancytechnology.com>
2017-03-20 18:40:46 +0530

    Paginate using will_paginate

---
+++ final/OneJobStreet/config/routes.rb
@@ -0,0 +1,42 @@
+Rails.application.routes.draw do
+
+
+  devise_for :employers, controllers: { application: 'application' }
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resource :employers, only: [:edit] do
+      collection do
+        patch 'update_password'
+      end
+    end
+
+
+    resources :employers
+    resources :companies
+    resources :jobs
+    get 'list', to: 'jobs#list', as: :alljob
+    resources :contact_people
+    resources :industry_types
+    resources :job_types
+    resources :roles
+    resources :cities
+
+  resources :experiences
+  resources :educations
+  resources :education_details
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  resources :users
+  root "users#home"
+  resource :skills
+  resources :locations
+  resources :education_details
+  resources :educations
+
+  resource :user, only: [:edit] do
+  collection do
+    patch 'update_password'
+  end
+end
+#post "/users/password" => "devise/passwords#create" ,as: "changepassword"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
